24e6d2014871a6295cd67c74754907b31c867e13
mtnmosesmayimela <moses.mayimela@mtn.com>
2020-05-18 20:47:25 +0200

    added unit tests

--- src/test/java/amazing/demo/DemoApplicationTests.java
+++ src/test/java/amazing/demo/DemoApplicationTests.java
@@ -1,5 +1,6 @@
 package amazing.demo;
 
+import static org.junit.jupiter.api.Assertions.fail;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -9,5 +10,13 @@ class DemoApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-
+        
+        @Test
+        public void testMain() {
+          try {
+            DemoApplication.main(new String[0]);
+          } catch (Exception e) {
+            fail("No exceptions should occur on startup.");
+          }
+        }
 }
