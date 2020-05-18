f22d52bf6e7f009e686fb2d73d8d0ff8435e8af7
mtnmosesmayimela <moses.mayimela@mtn.com>
2020-05-18 13:40:27 +0200

    Initial commit

---
+++ src/main/java/amazing/demo/DemoApplication.java
@@ -0,0 +1,24 @@
+package amazing.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+                
+        @GetMapping(
+            value = "/api/message",
+            produces = MediaType.APPLICATION_JSON_VALUE)
+        public String message() {
+
+            return "{\"Message\":\"hello sam\"}";
+        }
+}
