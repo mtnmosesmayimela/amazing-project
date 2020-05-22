95dcf8417872375063ac688cf3da11e45977c4e4
mtnmosesmayimela <moses.mayimela@mtn.com>
2020-05-22 08:27:03 +0200

    momomo

--- src/test/java/amazing/demo/DemoApplicationTests.java
+++ src/test/java/amazing/demo/DemoApplicationTests.java
@@ -26,7 +26,7 @@ class DemoApplicationTests {
         {
             DemoApplication instance= new DemoApplication();
             String result=instance.message();
-            String expected="{\"Message\":\"hello zazaza!\"}";           
+            String expected="{\"Message\":\"hello momomo!\"}";           
              assertEquals(expected, result);
         }
 }
